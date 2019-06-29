10b4b38fbc450c5518bb49ac670b952310fd0102
KIOT <kevindiner@outlook.com>
2019-06-29 21:36:59 +0200

    Extra config option and use of local icon

--- src/index.ts
+++ src/index.ts
@@ -38,16 +38,15 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
   // Channel for only joining, with nice formatted message. ENTER CHANNEL ID in config.ts
   const channeljoin = bot.channels.get(ConfigFile.config.channel_join); // 427146486623633420
 
-
-
   if (oldUserChannel === undefined && newUserChannel !== undefined) {
     // User Joins a voice channel
-     if(ConfigFile.config.mute_state_on_join === true){
+    if (ConfigFile.config.mute_state_on_join === true) {
       if (newMember.selfMute === true) {
         if (channellog) {
           channellog.send(newMember.displayName + " Is Muted.");
         }
-      } if (newMember.selfMute === false) {
+      }
+      if (newMember.selfMute === false) {
         // User unmutes himself
         if (channellog) {
           channellog.send(newMember.displayName + " Is Unmuted.");
@@ -55,14 +54,16 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
       }
     }
 
+    // if (channeljoin) {
+    //   let embcolor = "5838556";
 
-    if (channeljoin) {
-      let embcolor = "5838556";
+    //   if (newMember.displayName === "Kiotam") {
+    //     embcolor = "3447003";
+    //   }
+    // }
 
-      if (newMember.displayName === "Kiotam") {
-        embcolor = "3447003";
-      }
-    }
+    
+
 
     if (channellog) {
       channellog.send(
@@ -70,111 +71,82 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
       );
     }
 
-    // var tijd = Date.now();
+    if (ConfigFile.config.message_layout_join === "big") {
+      const attachment = new Discord.Attachment('./card_images/approved.png', 'approved.png');
+      const embed = new Discord.RichEmbed()
+        .setTitle(newMember.displayName + " Joined A Voicechannel")
+        .setAuthor(
+          "Room Notifications",
+          newMember.guild.iconURL
+        )
+        /*
+         * Alternatively 0x00ae86, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor(ConfigFile.config.embed_message_color)
+        .setDescription(
+          "Welcome to the **" + newUserChannel.name + "** Channel.🤔"
+        )
+        .setFooter("Message was sent on")
+        .setImage(newMember.user.displayAvatarURL)
+        .attachFile(attachment)
+        .setThumbnail('attachment://approved.png')
+        /*
+         * Takes a Date object, defaults to current date.
+         */
+        .setTimestamp()
+        .setURL("https://kiot.nl")
+        .addField(
+          newMember.displayName,
+          "Enjoy your stay **" + newMember.displayName + "**!"
+        );
 
-    // const embed = {
-    //   title: newMember.displayName + " Joined A Voicechannel",
-    //   description: "Welcome to the **" + newUserChannel.name + "** Channel.🤔",
-    //   url: "https://kiot.nl",
-    //   color: 5838556,
-    //   timestamp: tijd,
-    //   footer: {
-    //     icon_url:
-    //       "https://www.natuurmuseumbrabant.nl/fileadmin/Icons/clock_icon.png",
-    //     text: "Message was sent on"
-    //   },
-    //   thumbnail: {
-    //     url:
-    //       "https://cdn.pixabay.com/photo/2013/07/12/12/17/check-145512_960_720.png"
-    //   },
-    //   image: {
-    //     url: newMember.user.displayAvatarURL
-    //   },
-    //   author: {
-    //     name: "Room Notifications",
-    //     url: "https://kiot.nl",
-    //     icon_url:
-    //       "https://www.sccpre.cat/mypng/detail/77-779070_open-door-icon-png-white-png-download-front.png"
-    //   },
-    //   fields: [
-    //     {
-    //       name: newMember.displayName,
-    //       value: "Enjoy your stay **" + newMember.displayName + "**!"
-    //     }
-    //   ]
-    // };
+      /*
+       * Inline fields may not display as inline if the thumbnail and/or image is too big.
+       */
+      // .addField("Inline Field", "They can also be inline.", true)
+      /*
+       * Blank field, useful to create some space.
+       */
+      // .addBlankField(true)
+      // .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
-    // // Sends above contructed message
-    // channeljoin.send("", { embed });
-    
-    
+      channeljoin.send("", { embed });
+    }
 
 
-    const embed = new Discord.RichEmbed()
-    .setTitle(newMember.displayName + " Joined A Voicechannel")
-    .setAuthor("Room Notifications", "https://www.sccpre.cat/mypng/detail/77-779070_open-door-icon-png-white-png-download-front.png")
-    /*
-     * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-     */
-    .setColor(0x00AE86)
-    .setDescription("Welcome to the **" + newUserChannel.name + "** Channel.🤔")
-    .setFooter("Message was sent on")
-    .setImage(newMember.user.displayAvatarURL)
-    .setThumbnail("https://cdn.pixabay.com/photo/2013/07/12/12/17/check-145512_960_720.png")
-    /*
-     * Takes a Date object, defaults to current date.
-     */
-    .setTimestamp()
-    .setURL("https://kiot.nl")
-    .addField(newMember.displayName,
-      "Enjoy your stay **" + newMember.displayName + "**!")
-    /*
-     * Inline fields may not display as inline if the thumbnail and/or image is too big.
-     */
-    // .addField("Inline Field", "They can also be inline.", true)
-    /*
-     * Blank field, useful to create some space.
-     */
-    // .addBlankField(true)
-    // .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-   
-    channeljoin.send("", { embed });
+    // const attachment = new Discord.Attachment('./card_images/sample.png', 'sample.png');
+    // const embed = new Discord.RichEmbed()
+    //         .setTitle('Wicked Sweet Title')
+    //         .attachFile(attachment)
+    //         .setImage('attachment://sample.png')
+    //         channeljoin.send({embed}).catch(console.error)
 
 
 
 
 
+    if (ConfigFile.config.message_layout_join === "small") {
+      const embed = new Discord.RichEmbed()
+        .setTitle(newMember.displayName + " Joined A Voicechannel")
+        .setAuthor(
+          "Room Notifications",
+          newMember.guild.iconURL
+        )
+        .setColor(ConfigFile.config.embed_message_color)
+        .setDescription(
+          "Welcome to the **" + newUserChannel.name + "** Channel.🤔"
+        )
+        .setFooter("Message was sent on")
+        .setThumbnail(newMember.user.displayAvatarURL)
+        .setTimestamp()
+        .setURL("https://kiot.nl")
+        .addField(
+          newMember.displayName,
+          "Enjoy your stay **" + newMember.displayName + "**!"
+        );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      channeljoin.send("", { embed });
+    }
   } else if (newUserChannel === undefined) {
     // User leaves a voice channel or Mutes/ Unmutes
     //Sends Message in Channel log with which user left which channel
@@ -183,43 +155,28 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
         newMember.displayName + " Left! Channel: " + oldUserChannel.name
       );
     }
+  } else if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
+    // user moved from one voice channel to another (old channel ID is different from the new one)
+    bot.channels
+      .get(ConfigFile.config.channel_log)
+      .send(
+        `${newMember} moved from voice channel ${oldUserChannel} to ${newUserChannel}`
+      );
+  } else if (oldMember.voiceChannelID === newMember.voiceChannelID) {
+    //user stays in same channel (old channel ID is same from the new one), check if user did mute/unmute
+    // If nobody is joining or leaving check if somebody muted or unmuted
+    // User mutes himself
+
+    if (newMember.selfMute === true) {
+      if (channellog) {
+        channellog.send(newMember.displayName + " Is Muted.");
+      }
+    }
+    if (newMember.selfMute === false) {
+      // User unmutes himself
+      if (channellog) {
+        channellog.send(newMember.displayName + " Is Unmuted.");
+      }
+    }
   }
-
-   else if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
-  // user moved from one voice channel to another (old channel ID is different from the new one)
-  bot.channels.get(ConfigFile.config.channel_log).send(`${newMember} moved from voice channel ${oldUserChannel} to ${newUserChannel}`);
-} 
-   else if (oldMember.voiceChannelID === newMember.voiceChannelID) {
-   //user stays in same channel (old channel ID is same from the new one), check if user did mute/unmute
-     // If nobody is joining or leaving check if somebody muted or unmuted
-          // User mutes himself
-          
-          if (newMember.selfMute === true) {
-          if (channellog) {
-            channellog.send(newMember.displayName + " Is Muted.");
-          }
-        } if (newMember.selfMute === false) {
-          // User unmutes himself
-          if (channellog) {
-            channellog.send(newMember.displayName + " Is Unmuted.");
-          }
-        }
-
- }
-
 });
-
-
-
-
-// bot.on('voiceStateUpdate', (oldMember, newMember) => {
-//   let newUserChannel = newMember.voiceChannellet oldUserChannel = oldMember.voiceChannel;
-
-//   if(!oldUserChannel && newUserChannel) {
-//       // user joined a channel without being in one previously (old channel is undefined, new channel is defined)
-//       bot.channels.get('529566151731052566').send(`${newMember} joined voice channel ${newUserChannel}`);
-//   } else if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
-//       // user moved from one voice channel to another (old channel ID is different from the new one)
-//       bot.channels.get('529566151731052566').send(`${newMember} moved from voice channel ${oldUserChannel} to ${newUserChannel}`);
-//   }
-// });
