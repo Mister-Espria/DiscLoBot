93ddf09e45ac00e37bd134ba05f3a3970cd57141
Kevin <kevindiner@outlook.com>
2019-06-30 12:08:28 +0200

    Update config.js
    Added some info

--- dist/config.js
+++ dist/config.js
@@ -5,8 +5,8 @@ exports.config = {
     "prefix": "?",
     "channel_join": "ENTER_CHANNEL_ID_FOR_JOIN_ANNOUNCEMENTS_HERE",
     "channel_log": "5ENTER_CHANNEL_ID_FOR_LOGGING_HERE",
-    "mute_state_on_join": false,
-    "message_layout_join": "big",
-    "embed_message_color": "#00AE86"
+    "mute_state_on_join": false,                // if true; sends the muted/unmuted state when the user joins, in a message to channel_log. if false; message not send when user joins a voicechannel
+    "message_layout_join": "big",              // big for a message with a big picture of the user or small with only a thumbnail size picture of the user
+    "embed_message_color": "#00AE86"          //this is the hex color of the left bar of the message #BC0026
 };
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFHO0lBQ2hCLE9BQU8sRUFBRSw2REFBNkQ7SUFDdEUsUUFBUSxFQUFFLEdBQUc7SUFDYixjQUFjLEVBQUUsb0JBQW9CO0lBQ3BDLGFBQWEsRUFBRSxvQkFBb0I7SUFDbkMsb0JBQW9CLEVBQUUsS0FBSztJQUMzQixxQkFBcUIsRUFBRSxLQUFLO0lBQzVCLHFCQUFxQixFQUFFLFNBQVM7Q0FDbkMsQ0FBQSJ9
